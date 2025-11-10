c76958b5bfa17a54c5aed563898b33ec69547c69
Your Name <your.email@example.com>
2025-11-09 21:38:09 -0800

    eval fix

--- scripts/run-tim-30B.sh
+++ scripts/run-tim-30B.sh
@@ -66,8 +66,9 @@ ROLLOUT_ARGS=(
 )
 
 EVAL_ARGS=(
-   --eval-interval 20
+   --eval-interval 50
    --eval-prompt-data aime /lambda/nfs/aime-2024/aime-2024.jsonl
+   --eval-input-key prompt
    --n-samples-per-eval-prompt 16
    --eval-max-response-len 16384
    --eval-top-p 0.7
