9efc87420c91c1420d13dd3bad6b0c9020f71dc2
Your Name <your.email@example.com>
2025-11-10 19:05:31 -0800

    set the max connections to 32

--- scripts/run-qwen3-30B-A3B.sh
+++ scripts/run-qwen3-30B-A3B.sh
@@ -133,7 +133,7 @@ ray start --head --node-ip-address ${MASTER_ADDR} --num-gpus 8 --disable-usage-s
 RUNTIME_ENV_JSON="{
   \"env_vars\": {
     \"PYTHONPATH\": \"/root/Megatron-LM/\",
-    \"CUDA_DEVICE_MAX_CONNECTIONS\": \"1\",
+    \"CUDA_DEVICE_MAX_CONNECTIONS\": \"32\",
     \"NCCL_NVLS_ENABLE\": \"${HAS_NVLINK}\"
   }
 }"
